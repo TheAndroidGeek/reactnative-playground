4ef3fffa728a64ff70fc4ebf86105d0216d072f9
artishevskym <56644443+artishevskym@users.noreply.github.com>
2023-04-07 15:37:40 +0100

    Collect input from color form

--- ColorCatalog/App.js
+++ ColorCatalog/App.js
@@ -1,24 +1,30 @@
-import { StyleSheet, View, FlatList } from "react-native";
+import { StyleSheet, View, FlatList, Alert } from "react-native";
 import React, { useState } from "react";
 import ColorButton from "./components/ColorButton";
 import defaultColors from "./data/defaultColors.json";
+import ColorForm from "./components/ColorForm";
 
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState("blue");
   return (
-    <FlatList
-      data={defaultColors}
-      style={[styles.container, { backgroundColor }]}
-      renderItem={({ item }) => {
-        return (
-          <ColorButton
-            key={item.id}
-            backgroundColor={item.color}
-            onPress={setBackgroundColor}
-          />
-        );
-      }}
-    />
+    <>
+      <ColorForm
+        onNewColor={(newColor) => Alert.alert(`TODO: add color ${newColor}`)}
+      />
+      <FlatList
+        data={defaultColors}
+        style={[styles.container, { backgroundColor }]}
+        renderItem={({ item }) => {
+          return (
+            <ColorButton
+              key={item.id}
+              backgroundColor={item.color}
+              onPress={setBackgroundColor}
+            />
+          );
+        }}
+      />
+    </>
   );
 }
 
---
+++ ColorCatalog/components/ColorForm.js
@@ -0,0 +1,43 @@
+import React, { useState, useRef } from "react";
+import { StyleSheet, View, TextInput, Button } from "react-native";
+
+export default function ColorForm({ onNewColor = (f) => f }) {
+  const [inputValue, setValue] = useState("");
+  const input = useRef();
+  return (
+    <View style={styles.container}>
+      <TextInput
+        ref={input}
+        value={inputValue}
+        onChangeText={(text) => setValue(text)}
+        placeholder="enter a color..."
+        style={styles.txtInput}
+        autoCapitalize="none"
+      />
+      <Button
+        title="add"
+        onPress={() => {
+          input.current.blur();
+          onNewColor(inputValue);
+          setValue("");
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  txtInput: {
+    flex: 1,
+    borderWidth: 2,
+    fontSize: 20,
+    margin: 5,
+    borderRadius: 5,
+    padding: 5,
+  },
+});
