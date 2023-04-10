bf615175c0ff9a3480d60d87dfbf76ef8c907116
artishevskym <56644443+artishevskym@users.noreply.github.com>
2023-04-10 16:32:33 +0100

    Add navigation between screens

--- ColorCatalog/App.js
+++ ColorCatalog/App.js
@@ -1,37 +1,22 @@
-import { StyleSheet, View, FlatList, Alert } from "react-native";
-import React, { useState } from "react";
-import ColorButton from "./components/ColorButton";
-import defaultColors from "./data/defaultColors.json";
-import ColorForm from "./components/ColorForm";
-import { generate } from "shortid";
-import { useColors } from "./hooks";
+import React from "react";
+import ColorList from "./components/ColorList";
+import ColorDetails from "./components/ColorDetails";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const { Navigator, Screen } = createStackNavigator();
 
 export default function App() {
-  const [backgroundColor, setBackgroundColor] = useState("blue");
-  const { colors, addColor } = useColors();
   return (
-    <>
-      <ColorForm onNewColor={addColor} />
-      <FlatList
-        data={colors}
-        style={[styles.container, { backgroundColor }]}
-        renderItem={({ item }) => {
-          return (
-            <ColorButton
-              key={item.id}
-              backgroundColor={item.color}
-              onPress={setBackgroundColor}
-            />
-          );
-        }}
-      />
-    </>
+    <NavigationContainer>
+      <Navigator>
+        <Screen
+          name="Home"
+          options={{ title: "Color List" }}
+          component={ColorList}
+        />
+        <Screen name="Details" component={ColorDetails} />
+      </Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    display: "flex",
-  },
-});
